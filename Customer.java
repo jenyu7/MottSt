b6b1a96702bc31939387506cc2f2df01b9c2a3a7
Jen Yu <jyu7@stuy.edu>
2017-05-18 20:08:56 -0400

    customer class complete

--- Customer.java
+++ Customer.java
@@ -1,11 +1,16 @@
+//Class Customer (Vanilla)
 public class Customer
 {
+	//Instance Variables
+	private String name;
 	private int table#;
 	String[] dishes = {"chow mein", "chow fun", "chicken n broccoli", "duck soup", "lo mein", "soup dumplings"};
 	private ArrayList<Order> orders;
 	
+	//Constructor: populates order with random dishes
 	public Customer()
 	{
+		name = "Unknown Customer";
 		int size = (int) (Math.random() * 6);
 		while (size >= 0)
 		{
@@ -13,5 +18,58 @@ public class Customer
 			orders.add(new Order(dishes[index]);
 			size --;
 		}
+	}
+	
+	//Overloaded Constructor: sets a name for the Customer
+	public Customer(String s)
+	{
+		this();
+		name = s;
+	}
+	
+	//Prints out the Customer's name and her/his orders
+	public String toString()
+	{
+		String s = name + "\n";
+		for (Order o:orders)
+		{
+			s += o.getDishName() + ", ";
+		}
+		s = s.substring(0, s.length()-1);
+		return s;
+	}
+	
+	//Accesses the table# the customer is seated at
+	public int getTable()
+	{
+		return table#;
+	}
+	
+	//two customers are equal if they are sitting at the same table
+	public boolean equals(Customer c)
+	{
+		return this.getTable() == c.getTable();
+	}
+	
+	//adds an order for a Customer
+	public void addOrder(Order newOrder)
+	{
+		orders.add(newOrder);
+	}
+	
+	//removes an order after a Customer has been served it
+	public void removeOrder(Order oldOrder)
+	{
+		int index = 0;
+		for (Order o: orders)
+		{
+			if (o.equals(oldOrder))
+			{
+				orders.remove(index);
+				return index;
+			}
+			index ++;
+		}
+		return -1;
 	}
 }
--- Order.java
+++ Order.java
@@ -1,3 +1,17 @@
 public class Order
 {
+	/*
+	private String dishName
+	private int table#
+	----
+	Order(): defaults name to null and table# to 0. 
+
+	Order(String s, int num): sets the variable name to s and table# to num
+
+	String getName(): returns the name of the dish being ordered
+
+	int getTable(): returns the table number that placed the order
+
+	void setTable(int t): sets the table number to the parameter
+	*/
 }
