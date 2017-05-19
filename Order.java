c0c7fab68adf5c31d4ecb9c4aef736e6f0afd619
Jen Yu <jyu7@stuy.edu>
2017-05-19 11:20:53 -0400

    Order class compiles

--- Order.java
+++ Order.java
@@ -3,19 +3,19 @@ public class Order
 {
 	//Instance Variables
 	private String dishName;
-	private int table#;
+	private int tableNum;
 	
 	//Default Constructor
 	Order(String s, int num)
 	{
 		dishName = s;
-		table# = num;
+		tableNum = num;
 	}
 	
 	//Two orders are the same if they have the same name and the same table placed the order
 	public boolean equals(Order o)
 	{
-		return this.dishName.equals(o.dishName) && (this.table# == o.table#);
+		return this.dishName.equals(o.dishName) && (this.tableNum == o.tableNum);
 	}
 	
 	//returns the dishName
@@ -27,12 +27,12 @@ public class Order
 	//returns the table that placed the order
 	public int getTable()
 	{
-		return table#;
+		return tableNum;
 	}
 	
 	//sets the table# of the order, if it needs to change
 	public void setTable(int t)
 	{
-		table# = t;
+		tableNum = t;
 	}
 }
